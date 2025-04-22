42a4f92234e2e34e2b73b5492c531907454dac07
vctrubio <vctrubio@gmail.com>
2025-04-23 00:09:34 +0200

    notes for arquitechture, rpc-events todos

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -91,59 +91,12 @@ function initListener(
     };
 }
 
-//get Mint and Burn logs from the contract
-async function getFilterLogs(contract: Contract, user: User): Promise<GameEvent[]> {
-    const filterMint: ethers.Filter = {
-        address: contract.address,
-        fromBlock: 0,
-        toBlock: 'latest',
-        topics: [
-            ethers.id('Mint(address,uint256,uint256)'),
-        ],
-    };
-
-    const filterBurn: ethers.Filter = {
-        address: contract.address,
-        fromBlock: 0,
-        toBlock: 'latest',
-        topics: [
-            ethers.id('Burn(address,uint256,uint256)'),
-        ],
-    };
-
-    const [mintLogs, burnLogs] = await Promise.all([
-        getLogs(filterMint, user.provider),
-        getLogs(filterBurn, user.provider)
-    ]);
-
-    const parsedMintLogs = mintLogs.map((log) => {
-        const parsedLog = contract.instance?.interface.parseLog(log);
-        return {
-            address: parsedLog?.args[0],
-            tokenId: Number(parsedLog?.args[1]),
-            amount: Number(parsedLog?.args[2]),
-            type: 'mint',
-        } as GameEvent;
-    });
-
-    const parsedBurnLogs = burnLogs.map((log) => {
-        const parsedLog = contract.instance?.interface.parseLog(log);
-        return {
-            address: parsedLog?.args[0],
-            tokenId: Number(parsedLog?.args[1]),
-            amount: Number(parsedLog?.args[2]),
-            type: 'burn',
-        } as GameEvent;
-    });
-
-    const allEvents = [...parsedMintLogs, ...parsedBurnLogs];
-    return allEvents;
-}
-
 //we have the events, but now we ned to sort them by token id, and by user address
 //todo and to think
 
-
+//getFilterLogs that returns events.... and then what.
+//in initGameTheory or handle game theory we need to pass a param, that will be null if user doesnt ecxist. ie on the first mount,
+// -- this, if user is changed. we dont need the socket to change, if the socket doesnt change, then we dont need to reint the listener or the filterlogs....
 export const GameProvider = ({ children }: { children: ReactNode }) => {
     const [game, setGame] = useState<GameTheory | null>(null);
     const [events, setEvents] = useState<GameEvent[]>([]);
--- src/lib/rpc-events.ts
+++ src/lib/rpc-events.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-
+import { GameEvent } from "./game";
 
 export async function getLogs(
   filter: ethers.Filter,
@@ -7,4 +7,52 @@ export async function getLogs(
 ): Promise<ethers.Log[]> {
   const raw = await provider.getLogs(filter);
   return raw;
+}
+
+//get Mint and Burn logs from the contract
+export async function getFilterLogs(
+  contract: Contract,
+  user: User
+): Promise<GameEvent[]> {
+  const filterMint: ethers.Filter = {
+    address: contract.address,
+    fromBlock: 0,
+    toBlock: "latest",
+    topics: [ethers.id("Mint(address,uint256,uint256)")],
+  };
+
+  const filterBurn: ethers.Filter = {
+    address: contract.address,
+    fromBlock: 0,
+    toBlock: "latest",
+    topics: [ethers.id("Burn(address,uint256,uint256)")],
+  };
+
+  const [mintLogs, burnLogs] = await Promise.all([
+    getLogs(filterMint, user.provider),
+    getLogs(filterBurn, user.provider),
+  ]);
+
+  const parsedMintLogs = mintLogs.map((log) => {
+    const parsedLog = contract.instance?.interface.parseLog(log);
+    return {
+      address: parsedLog?.args[0],
+      tokenId: Number(parsedLog?.args[1]),
+      amount: Number(parsedLog?.args[2]),
+      type: "mint",
+    } as GameEvent;
+  });
+
+  const parsedBurnLogs = burnLogs.map((log) => {
+    const parsedLog = contract.instance?.interface.parseLog(log);
+    return {
+      address: parsedLog?.args[0],
+      tokenId: Number(parsedLog?.args[1]),
+      amount: Number(parsedLog?.args[2]),
+      type: "burn",
+    } as GameEvent;
+  });
+
+  const allEvents = [...parsedMintLogs, ...parsedBurnLogs];
+  return allEvents;
 }
