52c8012eaf844456549b64eed6d2d74938971c95
vctrubio <vctrubio@gmail.com>
2025-04-23 00:07:08 +0200

    implement initFilterLogs

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -2,15 +2,156 @@ import { useState, ReactNode, useEffect } from "react";
 import { ethers } from "ethers";
 import { GameContext } from "./GameContextDef";
 import { initGameTheory } from "../lib/gameTheory";
-import { getWallet, hasMetamask, getUserByProvider } from "../lib/ethers";
+import { hasMetamask, getUserByProvider } from "../lib/ethers";
 import { useNotifications } from "../hooks/useNotifications";
+import { GameEvent } from "../lib/game";
+import { getTokenById } from "../lib/utils";
+import { getLogs } from "../lib/rpc-events";
+
+function initListener(
+    contract: ethers.Contract,
+    addEventCallback: (newEvent: GameEvent) => void,
+    showNotification: any
+) {
+    contract.on(
+        "Mint",
+        (to: string, tokenId: number, amount: number, event: any) => {
+            const token = getTokenById(Number(tokenId));
+            if (!token) return;
+            const msg = `${to.substring(0, 2)}...${to.substring(
+                to.length - 3
+            )} minted ${token.name}`;
+            console.log(msg);
+            showNotification(msg, "success", 5000);
+
+            const newEvent: GameEvent = {
+                address: to,
+                tokenId: Number(tokenId),
+                amount: Number(amount),
+                type: "mint",
+                transactionHash: event.transactionHash,
+                blockNumber: event.blockNumber,
+            };
+
+            addEventCallback(newEvent);
+
+            // Dispatch a custom event that Game.tsx can listen to
+            const tokenUpdateEvent = new CustomEvent("tokenUpdate", {
+                detail: {
+                    type: "mint",
+                    address: to,
+                    tokenId: Number(tokenId),
+                    amount: Number(amount),
+                },
+            });
+            window.dispatchEvent(tokenUpdateEvent);
+        }
+    );
+
+    contract.on(
+        "Burn",
+        (from: string, tokenId: number, amount: number, event: any) => {
+            const token = getTokenById(Number(tokenId));
+            if (!token) return;
+            const msg = `${from.substring(0, 2)}...${from.substring(
+                from.length - 3
+            )} burned ${token.name}`;
+            console.log(msg);
+            showNotification(msg, "warning", 5000);
+
+            const newEvent: GameEvent = {
+                address: from,
+                tokenId: Number(tokenId),
+                amount: Number(amount),
+                type: "burn",
+                transactionHash: event.transactionHash,
+                blockNumber: event.blockNumber,
+            };
+
+            addEventCallback(newEvent);
+
+            // Dispatch a custom event that Game.tsx can listen to // to be decided
+            // this can be done through window.EventListener ... 
+            const tokenUpdateEvent = new CustomEvent("tokenUpdate", {
+                detail: {
+                    type: "burn",
+                    address: from,
+                    tokenId: Number(tokenId),
+                    amount: Number(amount),
+                },
+            });
+            window.dispatchEvent(tokenUpdateEvent);
+        }
+    );
+
+    // Return a cleanup function to remove listeners
+    return () => {
+        contract.removeAllListeners("Mint");
+        contract.removeAllListeners("Burn");
+    };
+}
+
+//get Mint and Burn logs from the contract
+async function getFilterLogs(contract: Contract, user: User): Promise<GameEvent[]> {
+    const filterMint: ethers.Filter = {
+        address: contract.address,
+        fromBlock: 0,
+        toBlock: 'latest',
+        topics: [
+            ethers.id('Mint(address,uint256,uint256)'),
+        ],
+    };
+
+    const filterBurn: ethers.Filter = {
+        address: contract.address,
+        fromBlock: 0,
+        toBlock: 'latest',
+        topics: [
+            ethers.id('Burn(address,uint256,uint256)'),
+        ],
+    };
+
+    const [mintLogs, burnLogs] = await Promise.all([
+        getLogs(filterMint, user.provider),
+        getLogs(filterBurn, user.provider)
+    ]);
+
+    const parsedMintLogs = mintLogs.map((log) => {
+        const parsedLog = contract.instance?.interface.parseLog(log);
+        return {
+            address: parsedLog?.args[0],
+            tokenId: Number(parsedLog?.args[1]),
+            amount: Number(parsedLog?.args[2]),
+            type: 'mint',
+        } as GameEvent;
+    });
+
+    const parsedBurnLogs = burnLogs.map((log) => {
+        const parsedLog = contract.instance?.interface.parseLog(log);
+        return {
+            address: parsedLog?.args[0],
+            tokenId: Number(parsedLog?.args[1]),
+            amount: Number(parsedLog?.args[2]),
+            type: 'burn',
+        } as GameEvent;
+    });
+
+    const allEvents = [...parsedMintLogs, ...parsedBurnLogs];
+    return allEvents;
+}
+
+//we have the events, but now we ned to sort them by token id, and by user address
+//todo and to think
+
 
 export const GameProvider = ({ children }: { children: ReactNode }) => {
     const [game, setGame] = useState<GameTheory | null>(null);
+    const [events, setEvents] = useState<GameEvent[]>([]);
     const { showNotification } = useNotifications();
 
     useEffect(() => {
         console.log("GameProvider mounted");
+
     }, []);
 
     useEffect(() => {
@@ -88,6 +229,20 @@ export const GameProvider = ({ children }: { children: ReactNode }) => {
         //qs: should showNotification be here?
     }, [game?.User, showNotification]);
 
+    useEffect(() => {
+        if (game?.ContractSocket?.instance) {
+            const addNewEvent = (newEvent: GameEvent) => {
+                setEvents(prevEvents => [...prevEvents, newEvent]);
+            };
+
+            const cleanup = initListener(game.ContractSocket.instance, addNewEvent, showNotification);
+
+            return () => {
+                if (cleanup) cleanup();
+            };
+        }
+    }, [game?.ContractSocket?.instance, showNotification]);
+
     const handleInitGameTheory = async (): Promise<GameTheory | null> => {
         try {
             const gameTheory = await initGameTheory();
@@ -105,10 +260,13 @@ export const GameProvider = ({ children }: { children: ReactNode }) => {
         }
     };
 
+    window.ge = events;
+
     return (
         <GameContext.Provider
             value={{
                 game,
+                events,
                 initGameTheory: handleInitGameTheory,
                 networkId: game?.User?.network.id,
             }}
--- src/contexts/GameContextDef.tsx
+++ src/contexts/GameContextDef.tsx
@@ -1,7 +1,8 @@
 import { createContext } from 'react';
-
+import { GameEvent } from '../lib/game';
 interface GameContextType {
     game: GameTheory | null;
+    events: GameEvent[];
     initGameTheory: () => Promise<GameTheory | null>;
     networkId: string | undefined;
 }
